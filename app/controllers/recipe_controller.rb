89b022afbb2ce4965584cdc2ff4568f92a0d4963
Nolan Chan <nolanchan@berkeley.edu>
2014-10-13 15:49:27 -0700

    Created User and Recipe Controller

---
+++ app/controllers/recipe_controller.rb
@@ -0,0 +1,24 @@
+class RecipeController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def search
+  end
+
+end
