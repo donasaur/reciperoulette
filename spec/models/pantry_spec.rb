6a648b0a8938d0f2e3be6f341b25c13b1127cd07
Robinson <robinsonkuo@gmail.com>
2014-10-23 13:36:48 -0700

    Added a single unit test and commented out placeholder code.

--- spec/models/pantry_spec.rb
+++ spec/models/pantry_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Pantry, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#RSpec.describe Pantry, :type => :model do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+#RSpec.describe User, :type => :model do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
+
+
+
+describe "validations" do
+  before(:each) do
+    @user = User.new(email: "obama@whitehouse.gov", password: 'password', password_confirmation: 'password')
+  end
+
+  it "should accept a valid username-password pair" do
+    expect(@user.valid?).to eq true
+  end
+
 end
