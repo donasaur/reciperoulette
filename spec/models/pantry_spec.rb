5b4bf69bfa0830246f7c721f9a33129af614a542
Nolan Chan <nolanchan@berkeley.edu>
2014-10-24 15:24:29 -0700

    Added unit tests for pantry and recipe

--- spec/models/pantry_spec.rb
+++ spec/models/pantry_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-#RSpec.describe Pantry, :type => :model do
-#  pending "add some examples to (or delete) #{__FILE__}"
-#end
+RSpec.describe Pantry, :type => :model do
+  before :each do
+    @u = User.create(email: "obama@whitehouse.gov", password: 'password', password_confirmation: 'password')
+    u_id = @u.id
+    @p = Pantry.new(user_id: u_id)
+  end
+
+  it "cannot exist without a user" do
+    p = Pantry.new(user_id: nil)
+    expect(p.save).to eq false
+  end
+
+  it "should be able to be created when tied to a user" do
+    expect(@p.save).to eq true
+  end
+
+  it "may have ingredients" do
+    i = Ingredient.create(name: "chicken")
+    @p.ingredient_ids = @p.ingredient_ids << i.id
+    expect(@p.ingredient_ids.length).to eq 1
+  end
+
+  it "may not have repeated ingredients in the pantry" do
+    i = Ingredient.create(name: "chicken")
+    @p.ingredient_ids = @p.ingredient_ids << i.id
+    @p.ingredient_ids = @p.ingredient_ids << i.id
+    expect(@p.ingredient_ids.length).to eq 1
+  end
+
+end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -9,5 +9,32 @@ RSpec.describe Recipe, :type => :model do
     expect(@recipe.to_s).to eq @recipe.name
   end
 
+  it "cannot have a blank name" do
+    recipe = Recipe.new(name: "")
+    expect(recipe.save).to eq false
+  end
+
+  it "should have a unique name" do
+    recipe1 = Recipe.create(name: "chicken")
+    recipe2 = Recipe.new(name: "chicken")
+    expect(recipe2.save).to eq false
+  end
+
+  it "should properly handle creation of multiple recipes (3)" do
+    recipe1 = Recipe.create(name: "chicken noodle soup")
+    recipe2 = Recipe.create(name: "split pea soup")
+    recipe3 = Recipe.create(name: "vegetable garden soup")
+    expect(Recipe.all.length).to eq 3
+  end
+
+  it "should be able to have instructions, prep_time, cook_time, and description" do
+    recipe = Recipe.create(name: "chicken noodle soup", tag: "chicken", prep_time: 10, cook_time: 20, description: "Yummy soup")
+    r = Recipe.first
+    expect(Recipe.all.length).to eq 1
+    expect(r.prep_time).to eq 10
+    expect(r.cook_time).to eq 20
+    expect(r.description).to eq "Yummy soup"
+  end
+
 end
 
