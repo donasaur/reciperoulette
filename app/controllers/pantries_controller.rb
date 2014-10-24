13a5106c8539c15f5e9e6c84bdd64416bd91b4b5
Nolan Chan <nolanchan@berkeley.edu>
2014-10-24 01:34:42 -0700

    Adding Ingredients works properly

--- app/controllers/pantries_controller.rb
+++ app/controllers/pantries_controller.rb
@@ -1,14 +1,16 @@
 class PantriesController < ApplicationController
   def update
-    ingredient_ids = params["pantry"]["ingredient_ids"].map(&:to_i)
-    puts "params ingredients ids"
-    puts ingredient_ids
     @user = current_user
     @pantry = @user.pantry
-    @pantry.ingredient_ids = @pantry.ingredient_ids - ingredient_ids
-    puts "pantry ingredient ids"
-    puts @pantry.ingredient_ids
-    @pantry.save
-    render "user/dashboard"
+    if params["commit"] == "Add Ingredient"
+      added_ingredient = params["ingredient"].to_i
+      @pantry.ingredient_ids = @pantry.ingredient_ids << added_ingredient
+      render "user/dashboard"
+    elsif params["commit"] == "Delete Ingredients"
+      ingredient_ids = params["pantry"]["ingredient_ids"].map(&:to_i)
+      @pantry.ingredient_ids = @pantry.ingredient_ids - ingredient_ids
+      @pantry.save
+      render "user/dashboard"
+    end
   end
 end
