0d22c30ff763c877555381d0ae3f15d85f6c3d12
Robinson <robinsonkuo@gmail.com>
2014-10-23 23:01:47 -0700

    Added unit and functional tests for user, ingredient and recipe

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ActiveRecord::Base
   has_and_belongs_to_many :recipes
-
+  
   def to_s
   	name
   end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-#RSpec.describe Recipe, :type => :model do
-#  pending "add some examples to (or delete) #{__FILE__}"
-#end
+RSpec.describe Recipe, :type => :model do
+  before(:each) do
+    @recipe = Recipe.new(name: "soup")
+  end
+
+  it "should have to_s equal to the name of the Recipe" do
+    expect(@recipe.to_s).to eq @recipe.name
+  end
+
+end
+
