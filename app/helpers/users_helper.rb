c4c8307b6182ac46034ab7134068cd49e17dd5d7
donhmai <donmai@berkeley.edu>
2014-11-02 12:52:51 -0800

    Added progress bar percentage to view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  # given a recipe, returns a percent of ingredients matched
+  def progress_bar_percentage(recipe)
+    num_of_ingredients_matched = (recipe.ingredients & current_user.pantry.ingredients).length
+    percent_of_ingredients_matched = num_of_ingredients_matched.fdiv(recipe.ingredients.length) * 100
+  end
 end
