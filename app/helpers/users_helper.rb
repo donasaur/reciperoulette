0f0b3375ff816f4df7e75f6fc1ab4c4343b71dbe
akittiudom <akittiudom@berkeley.edu>
2014-11-03 21:42:49 -0800

    added color coded frame based on percentage on roulette page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,5 +4,15 @@ module UsersHelper
   def progress_bar_percentage(recipe)
     num_of_ingredients_matched = (recipe.ingredients & current_user.pantry.ingredients).length
     percent_of_ingredients_matched = num_of_ingredients_matched.fdiv(recipe.ingredients.length) * 100
+    case percent_of_ingredients_matched
+    when 0..25
+      return "rgba(255,0,0,0.3)"
+    when 25..50
+      return "rgba(255,165,0,0.3)"
+    when 50..75
+      return "rgba(255,255,0,0.3)"
+    when 75..100
+      return "rgba(0,100,0,0.3)"
+    end
   end
 end
