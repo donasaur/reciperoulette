1d7a6a4b3440616ccb2d11ea23b8ecd2b960478e
akittiudom <akittiudom@berkeley.edu>
2014-11-07 15:31:46 -0800

    added instructions functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -105,7 +105,15 @@ recipe_one = Recipe.create({name: 'Roast Chicken',
                         prep_time:20,
                         tags: Tag.where(name: ['lunch', 'dinner']),
                         image: File.new("app/assets/images/Roast_Chicken.jpg"),
-                        ingredients: Ingredient.where(:name => ['whole chicken', 'salt', 'pepper', 'paprika', 'onion', 'celery', 'carrot', 'garlic', 'bay leaf', 'olive oil'])})
+                        ingredients: Ingredient.where(:name => ['whole chicken', 'salt', 'pepper', 'paprika', 'onion', 'celery', 'carrot', 'garlic', 'bay leaf', 'olive oil']),
+                        instructions: "Preheat oven to 400 degrees.\n
+                                Remove and discard insides of chicken. Rinse chicken with cold water and pat dry. Trim any excess fat.\n
+                                Combine salt, pepper, and paprika in a small bowl and sprinkle over chicken and into the cavity.\n
+                                Place vegetables and bay leaf in body cavity. Tie ends of legs together with a cord (I have never done this and it still turns out fine). Lift wing tips up and over back; tuck under chicken.\n
+                                Place chicken, breast side up, on a broiler pan (or on a rack inside another pan) coated with olive oil.\n
+                                Bake at 400 degrees for one hour or until meat thermometer inserted into thigh registers 180 degrees.\n
+                                Cover loosely with foil for about 10 minutes.\n
+                                Remove vegetables from inside chicken and throw them away."})
 recipe_two = Recipe.create(name: 'Scrambled Eggs',
                         cook_time: 5,
                         prep_time: 5,
