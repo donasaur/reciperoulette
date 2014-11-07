088dbd90f5c7a861e2385b3bf1c56f6f9b04389b
Nolan Chan <nolanchan@berkeley.edu>
2014-11-06 19:12:09 -0800

    Autocomplete functional on adding ingredients

--- app/assets/javascripts/pantries.js
+++ app/assets/javascripts/pantries.js
@@ -1,8 +1,17 @@
 jQuery(function() {
+
   return $('#ingredients').sortable({
     axis: 'y',
     update: function() {
       return $.post($(this).data('update-url'), $(this).sortable('serialize'));
     }
+  });
+});
+
+
+jQuery(function() {
+
+  return $('#ingredient_name').autocomplete({
+    source: $('#ingredient_name').data('autocomplete-source')
   });
 });
