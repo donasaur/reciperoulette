3a62a583c2a9bde25a9e0fc9a4ae794c186dac13
akittiudom <akittiudom@berkeley.edu>
2014-11-07 16:29:10 -0800

    added instructions to most recipes and got rid of colored boxes in recipe view

--- db/seeds.rb
+++ db/seeds.rb
@@ -114,40 +114,62 @@ recipe_one = Recipe.create({name: 'Roast Chicken',
                                 Bake at 400 degrees for one hour or until meat thermometer inserted into thigh registers 180 degrees.\n
                                 Cover loosely with foil for about 10 minutes.\n
                                 Remove vegetables from inside chicken and throw them away."})
-recipe_two = Recipe.create(name: 'Scrambled Eggs',
+recipe_two = Recipe.create({name: 'Scrambled Eggs',
                         cook_time: 5,
                         prep_time: 5,
                         tags: Tag.where(name: ['breakfast']),
                         image: File.new("app/assets/images/Scrambled_Eggs.jpg"),
-                        ingredients: Ingredient.where(:name => ['egg', 'butter', 'salt', 'pepper']))
+                        ingredients: Ingredient.where(:name => ['egg', 'butter', 'salt', 'pepper']), 
+                        instructions: "Break eggs into a bowl.\n
+                                Beat eggs until blended.\n
+                                Heat butter in a large nonstick skillet over medium heat until hot.\n
+                                Pour the eggs into the pan. As eggs begin to set, gently pull the eggs across the pan with a spatula. Continue to cook until thicken and no visible liquid remains.\n
+                                Remove from heat, add salt and pepper to taste and serve."})
 
-recipe_three = Recipe.create(name: 'Spaghetti',
+recipe_three = Recipe.create({name: 'Spaghetti',
                         cook_time: 15,
                         prep_time: 5,
                         tags: Tag.where(name: ['lunch', 'dinner']),
                         image: File.new("app/assets/images/Spaghetti.jpg"),
-                        ingredients: Ingredient.where(:name => ['spaghetti', 'tomato sauce', 'pepper', 'salt']))
+                        ingredients: Ingredient.where(:name => ['spaghetti', 'tomato sauce', 'pepper', 'salt']),
+                        instructions: "Bring a large pot of salted water to a rolling boil.\n
+                                Add dry spaghetti noodles to the boiling water. Cook for 10 minutes, stirring occassionally.\n
+                                Drain noodles and add to a sauce pan with the tomato sauce. Mix thoroughly and heat until hot.\n
+                                Remove from heat and add salt and pepper to taste."})
 
-recipe_four = Recipe.create(name: 'Chicken Fajitas',
+recipe_four = Recipe.create({name: 'Chicken Fajitas',
                         cook_time: 10,
                         prep_time: 10,
                         tags: Tag.where(name: ['lunch', 'dinner']),
                         image: File.new("app/assets/images/Chicken_Fajitas.jpg"),
-                        ingredients: Ingredient.where(:name => ['chicken breast', 'onion', 'bell pepper', 'salt', 'pepper']))
+                        ingredients: Ingredient.where(:name => ['chicken breast', 'onion', 'bell pepper', 'salt', 'pepper']),
+                        instructions: "Heat a grill pan to medium. Once hot, add the chicken breast pieces, season with salt and pepper, and cook undisturbed until well browned on the bottom, about 10 minutes.\n
+                                Flip, season the second side with salt and pepper, and cook undisturbed until well browned on the bottom and cooked through, about 10 minutes more. Remove the chicken to a cutting board and let it rest while you prepare the remaining ingredients.\n
+                                Place the bell pepper and onion in a medium bowl, drizzle with the remaining 1 tablespoon oil, season with salt and pepper, and toss to coat.\n
+                                Place the vegetables on the grill pan in a single layer. Cook, stirring occasionally, until tender and slightly charred, about 10 minutes. Transfer the vegetables to a serving dish.\n
+                                Slice the chicken against the grain into 1/2-inch-thick pieces and place in the serving dish with the vegetables."})
 
-recipe_five = Recipe.create(name: 'Quesadilla',
+recipe_five = Recipe.create({name: 'Quesadilla',
                         cook_time: 5,
                         prep_time: 1,
                         tags: Tag.where(name: ['lunch', 'dinner']),
                         image: File.new("app/assets/images/Quesadilla.jpg"),
-                        ingredients: Ingredient.where(:name => ['cheese', 'tortilla']))
+                        ingredients: Ingredient.where(:name => ['cheese', 'tortilla']),
+                        instructions: "Heat a large pan on medium heat until hot.\n
+                                Place one totilla down until warm and bubbly.\n
+                                Flip the totilla over, and generously sprinkle cheese to cover it. Then place the second tortilla on top of the cheese.\n
+                                When the bottom tortilla is golden brown, flip the quesdailla over and cook the second totilla utnil golden brown."})
 
-recipe_six = Recipe.create(name: 'French Toast',
+recipe_six = Recipe.create({name: 'French Toast',
                         cook_time: 5,
                         prep_time: 10,
                         tags: Tag.where(name: ['breakfast']),
                         image: File.new("app/assets/images/French_Toast.jpg"),
-                        ingredients: Ingredient.where(:name => ['egg', 'bread', 'butter', 'sugar', 'cinnamon', 'syrup']))
+                        ingredients: Ingredient.where(:name => ['egg', 'bread', 'butter', 'sugar', 'cinnamon', 'syrup', 'milk']),
+                        instructions: "Beat egg, sugar and cinnamon in shallow dish. Stir in milk.\n
+                                Dip bread in egg mixture, turning to coat both sides evenly.\n
+                                Cook bread slices on lightly buttered nonstick griddle or skillet on medium heat until browned on both sides.\n
+                                Serve with syrup."})
 
 recipe_six = Recipe.create(name: 'Ham and Egg Fried Rice',
                         cook_time: 10,
