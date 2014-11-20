493aa98cf21ea40d3fb9bc4712af1cc7c2dfacc0
akittiudom <akittiudom@berkeley.edu>
2014-11-19 21:13:08 -0800

    removed ul indentations, highlight ingredients that user has on recipe pages

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,12 @@
 module RecipesHelper
+
+  def recipe_ingredient_in_pantry(ingredient, user = current_user)
+    active_ingredients = get_active_ingredients(user)
+    if active_ingredients.include?(ingredient)
+      return "green"
+    else 
+      return "black"
+    end
+  end
+
 end
