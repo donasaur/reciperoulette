5b6c7139d40f1fac11b63c0eaa68542f0dd0af4d
Robinson <robinsonkuo@gmail.com>
2014-11-24 04:42:10 -0800

    Added recipe controller tests.

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -15,4 +15,29 @@ RSpec.describe RecipesController, :type => :controller do
     get :show, id: r.id
     expect(response).to render_template("recipes/show")
   end
+
+  it "should not accept recipe without tags" do
+    post :create, {name: 'Will fail before it matters'}
+    expect(response).to render_template("recipes/new")
+    expect(flash[:alert]).to eq "Failed to add new recipe - Please select at least one tag."
+  end
+
+  it "should not accept recipes without ingredients" do
+    post :create, {tags: { tag_ids: [1, 2, 3] }}
+    expect(response).to render_template("recipes/new")
+    expect(flash[:alert]).to eq "Failed to add new recipe - Need at least one ingredient."
+  end
+
+  it "should not accept ingredients missing in the database" do
+    post :create, {tags: { tag_ids: [1, 2, 3] }, ingredients: ['fake', '', '']}
+    expect(response).to render_template("recipes/new")
+    expect(flash[:alert]).to eq "Failed to add new recipe - Some ingredients are missing from our database: fake. You can add new ingredients on your dashboard."
+  end
+
+  it "should fail if only empty ingredients are submitted" do
+    post :create, {tags: { tag_ids: [1, 2, 3] }, ingredients: ['', '', '']}
+    expect(response).to render_template("recipes/new")
+    expect(flash[:alert]).to eq "Failed to add new recipe - Need at least one ingredient."
+  end
+
 end
